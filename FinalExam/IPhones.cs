67038ea382f744690be49545fe7a0af2601bc4fb
Sevvoo <111128831+Sevvoo@users.noreply.github.com>
2022-08-16 13:03:40 +0300

    Интерфейсы

---
+++ FinalExam/IPhones.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalExam
+{
+    interface IPhones : IAllCataloge
+    {
+        public string Phone_Model { get; set; }
+        public string Phone_Brand { get; set; }
+        public double Phone_Size { get; set; }
+        public int Phone_YearMade { get; set; } 
+        public double Price { get; set; }
+        public string Print();
+
+    }
+}
